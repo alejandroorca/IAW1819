77916ccd96c8ea540e2fe299029b1408cfd952d5
alejandroorca <alejandro96esp@gmail.com>
2018-10-25 21:47:42 +0200

    Subida 4 correcto

--- EjerciciosArrays/src/es/cj/arrays/Ejercicio4.java
+++ EjerciciosArrays/src/es/cj/arrays/Ejercicio4.java
@@ -1,29 +1,58 @@
 package es.cj.arrays;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio4 {
+
 	private static Scanner sc = new Scanner(System.in);
+	
+	// Leer 2 tablas de n números y mezclarla en una tercera tabla de la siguiente forma:
+	// 1º de A, 1º de B, 2º de A, 2º de B, 3º de A, 3º de B,...
+	
 	public static void main(String[] args) {
-		//4 Leer 2 tablas de n números y mezclarla en una tercera tabla de la siguiente forma: 
-		//1º de A, 1º de B, 2º de A, 2º de B, 3º de A, 3º de B,...
 		
-		System.out.println("Introduzca el número de valores a crear: ");
-		int numero = sc.nextInt();
-		int num = 0;
-		char abc [] = new char [27];
-		int numeros [] = new int [numero];
-		String ultima [] = new String [numero+27];		
-		for (char a = 'a'; a <= 'z'; a++) {
-			abc[num] = a;
-			num++;
-		}
-		for (int i = 1; i < numeros.length; i++) {
-			numeros[i] = i;
-		}
-		for (int j = 0; j < ultima.length; j++) {
-			ultima[j] = (numeros[j]+abc[j]); 
+		System.out.print("Cantidad de números en la tabla : ");
+		int cantidad = sc.nextInt();
+		int numeros1 [] = new int [cantidad];
+		for (int i = 0; i < numeros1.length; i++) {
+			numeros1[i] = new Random().nextInt(cantidad);
 		}
 		
-}
+		int cont1 = 0;
+		int cont2 = 0;
+		
+		int numeros2 [] = new int [cantidad];
+		for (int i = 0; i < numeros2.length; i++) {
+			numeros2[i] = new Random().nextInt(cantidad);
+		}
+		
+		int maxima = numeros1.length + numeros2.length;
+		int nuevaT [] = new int [maxima];
+				
+		for (int i = 0; i < nuevaT.length; i++) {
+			if(i%2==0) {
+				nuevaT[i]=numeros1[cont1];
+				cont1+=1;
+			}else if(i%2==1) {
+				nuevaT[i]=numeros2[cont2];
+				cont2+=1;
+			}
+		}
+		System.out.println("Tabla primera");
+		for (int i = 0; i < numeros1.length; i++) {
+			System.out.print(numeros1[i]+" ");
+		}
+		System.out.println(" ");
+		System.out.println("Tabla segunda");
+		for (int i = 0; i < numeros2.length; i++) {
+			System.out.print(numeros2[i]+" ");
+		}
+		System.out.println();
+		System.out.println("Tabla aleatoria");
+		System.out.println(Arrays.toString(nuevaT));
+		
+	}
+
 }
