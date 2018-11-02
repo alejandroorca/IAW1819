919053b69e78cdebe346db546b540a10910e711b
Alejandro <Alejandro@DESKTOP-J85RT5K>
2018-11-02 12:42:25 +0100

    Upload 02/11/2018 - 2.

--- EjerciciosArrays/src/es/cj/arrays/Ejercicio7.java
+++ EjerciciosArrays/src/es/cj/arrays/Ejercicio7.java
@@ -6,61 +6,67 @@ import java.util.Scanner;
 
 public class Ejercicio7 {
 	private static Scanner sc = new Scanner(System.in);
-	private static int alumnos [][];
+	private static int alumnos[][];
 	private static double mediasT[];
 	private static double mediasA[];
+
 	public static void main(String[] args) {
 		int numAlumnos = 0;
 		do {
 			System.out.println("Nº de alumnos: ");
 			numAlumnos = sc.nextInt();
 		} while (numAlumnos <= 0);
-	int trimestres = 0;
-	do {
-		System.out.println("Nº de trimestres: ");
-		trimestres = sc.nextInt();
-	} while (trimestres <=0);
-	alumnos = new int [numAlumnos][trimestres];
-	mediasT = new double [alumnos[0].length];
-	mediasA = new double [alumnos.length];
-	
-	leerNotas();
+		int trimestres = 0;
+		do {
+			System.out.println("Nº de trimestres: ");
+			trimestres = sc.nextInt();
+		} while (trimestres <= 0);
+		alumnos = new int[numAlumnos][trimestres];
+		mediasT = new double[alumnos[0].length];
+		mediasA = new double[alumnos.length];
+
+		leerNotas();
 		mostrarNotas();
 		calcularMedias();
 		mostrarMedias();
 	}
+
 	private static void mostrarMedias() {
 		System.out.println("--- MEDIAS ---");
 		System.out.println(Arrays.toString(mediasA));
 		System.out.println(Arrays.toString(mediasT));
 	}
+
 	private static void mostrarNotas() {
 		System.out.println("--- NOTAS ---");
 		for (int i = 0; i < alumnos.length; i++) {
 			for (int j = 0; j < alumnos[i].length; j++) {
-				System.out.println(alumnos[i][j]+ " ");
+				System.out.println(alumnos[i][j] + " ");
 			}
 			System.out.println();
 		}
-		
+
 	}
+
 	private static void calcularMedias() {
 		for (int i = 0; i < alumnos.length; i++) {
 			for (int j = 0; j < alumnos[i].length; j++) {
 				mediasT[j] += alumnos[i][j];
 				mediasA[i] += alumnos[i][j];
 			}
-			mediasA[i] = Math.rint((mediasA[i] / mediasT.length)*100)/100;
+			mediasA[i] = Math.rint((mediasA[i] / mediasT.length) * 100) / 100;
 		}
 		calcularMediasTrimestres();
-		
+
 	}
+
 	private static void calcularMediasTrimestres() {
 		for (int i = 0; i < mediasT.length; i++) {
-			mediasT[i] = Math.rint((mediasT[i] / mediasA.length)*100)/100;
+			mediasT[i] = Math.rint((mediasT[i] / mediasA.length) * 100) / 100;
 		}
-		
+
 	}
+
 	private static void leerNotas() {
 		for (int i = 0; i < alumnos.length; i++) {
 			for (int j = 0; j < alumnos[i].length; j++) {
@@ -68,7 +74,6 @@ public class Ejercicio7 {
 				System.out.println(alumnos[i][j]);
 			}
 		}
-		
+
 	}
 }
-
