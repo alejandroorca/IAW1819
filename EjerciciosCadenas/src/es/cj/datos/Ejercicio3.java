fe474271ab0d9a2c9629f883733887691a262e3e
alejandroorca <alejandro96esp@gmail.com>
2018-11-11 22:01:03 +0100

    Upload 11/11/2018 - Exercises Strings 3,4 and 5 is in process.

--- EjerciciosCadenas/src/es/cj/datos/Ejercicio3.java
+++ EjerciciosCadenas/src/es/cj/datos/Ejercicio3.java
@@ -6,9 +6,8 @@ public class Ejercicio3 {
 //Aplicar las operaciones básicas de modificación de Strings a la cadena “Uno dosuno”, 
 //añadiendo caracteres al final, cambiando la “o” por “e” y pasándola a mayúscula.
 		String cadena1 = "Uno dosuno";
-		String ultimo = cadena1.substring(cadena1.length()-1);
-		cadena1 = cadena1.replace(ultimo, "e");
-		cadena1 = cadena1.replace(ultimo, cadena1.toUpperCase());
+		cadena1 = cadena1.replace(cadena1.substring(cadena1.length()-1), "e");
+		cadena1 = cadena1.toUpperCase();
 		System.out.println(cadena1);
 	}
 
