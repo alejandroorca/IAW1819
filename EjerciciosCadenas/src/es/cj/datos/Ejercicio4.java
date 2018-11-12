e33046398f20b918c50894c98fe3830727a604f0
Alejandro <Alejandro@DESKTOP-J85RT5K>
2018-11-12 11:15:05 +0100

    Upload 12/11/2018

--- EjerciciosCadenas/src/es/cj/datos/Ejercicio4.java
+++ EjerciciosCadenas/src/es/cj/datos/Ejercicio4.java
@@ -5,28 +5,30 @@ import java.util.Arrays;
 public class Ejercicio4 {
 private static int suma = 0;
 private static int cont = 0, valor = 0;
-private static int A [] = new int [10];
+//private static int A [] = new int [10];
 	public static void main(String[] args) {
 		//Escriba un método que, dada una cadena de caracteres, de la suma de todos los dígitos que hay en ella. 
 		//Por ejemplo, si la cadena es abc12de3f4, dará como resultado 10.	
 		String cadena = "abc12de3f4";
 		System.out.println("La cadena es: "+cadena);
 		SumaNumeros(cadena);
-		System.out.println(suma);
+		System.out.println(valor);
 	}
 
 	private static void SumaNumeros(String cadena) {
 		 for(int i=0; i < cadena.length(); i++){
 	         if (Character.isDigit(cadena.charAt(i))){
 	        	 valor = valor + Character.getNumericValue(cadena.charAt(i));
-	        	 A[cont]= valor;
-	             valor = 0;
-	             cont++;
+	        	// A[cont]= valor;
+	             //valor = 0;
+	             //cont++;
+	        	 
 	         }
+	         
 	}
-		 for (int i = 0; i < A.length; i++) {
-			suma = suma + A[i];
+	//	 for (int i = 0; i < A.length; i++) {
+	//		suma = suma + A[i];
 		}
 
 	}
-}
+//}
--- EjerciciosCadenas/src/es/cj/datos/Ejercicio5.java
+++ EjerciciosCadenas/src/es/cj/datos/Ejercicio5.java
@@ -1,15 +1,23 @@
 package es.cj.datos;
 
+import java.util.Arrays;
+
 public class Ejercicio5 {
 	
+	private static char abc [] = new char [26];
 	public static void main(String[] args) {
 		// 5.Dada una cadena y un código entero aleatorio. 
 		//Generar una cadena cifrada donde cada carácter se sustituye por el carácter situado n posiciones después del mismo. 
 		//Debe empezar por la letra A si la suma del carácter con el codigo es mayor que Z.
 		
-		String cadena = "Hola qué tal";
-		String cadena1 = "1256754";
-		
+		CrearABC();
+		String cadenaf= "";
+	}
+	private static void CrearABC() {
+		for (int i = 0; i < abc.length; i++) {
+			abc[i] = (char) ('a' + i);
+			System.out.print(abc[i]+ " ");
+}		
 	}
 
 }
