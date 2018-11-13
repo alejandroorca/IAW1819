29b8318e550abd4bcd47ceee355fcb0b9e900151
alejandroorca <alejandro96esp@gmail.com>
2018-11-13 19:28:52 +0100

    Upload

--- EjerciciosCadenas/src/es/cj/datos/Ejercicio5.java
+++ EjerciciosCadenas/src/es/cj/datos/Ejercicio5.java
@@ -9,6 +9,7 @@ public class Ejercicio5 {
 	private static String cadena;
 	private static String cadenan;
 	private static char abc [] = new char [26];
+	private static int posicion;
 	public static void main(String[] args) {
 		// 5.Dada una cadena y un código entero aleatorio. 
 		//Generar una cadena cifrada donde cada carácter se sustituye por el carácter situado n posiciones después del mismo. 
@@ -22,15 +23,25 @@ public class Ejercicio5 {
 			cadenan = sc.nextLine();
 		} while (cadenan.isEmpty() || cadenan.length() > cadena.length());
 		CrearABC();
-		ComparaYResuelve(cadena);
+		System.out.println(ComparaYResuelve(cadena));
 	}
-	private static void ComparaYResuelve(String cadena2) {
+	private static String ComparaYResuelve(String cadena2) {
 		for (int i = 0; i < cadena.length(); i++) {
-			char caracter =;
-			if (cadena.substring(i).equalsIgnoreCase(abc[i])) {
-				
+			char caracter = cadena.charAt(i);
+			if (caracter == abc[i]) {
+			posicion = i + Character.getNumericValue(cadenan.charAt(i));
+			if (posicion < 27 && posicion >= 0) {
+				cadenaf =""+abc[posicion];
+				posicion = 0;	
+			} else if (posicion >= 27) {
+				posicion = Character.getNumericValue(cadenan.charAt(i));
+				cadenaf =""+abc[posicion];
+				posicion=0;
+			}
+			
 			} 
 		}
+		return cadenaf;
 		
 	}
 	private static void CrearABC() {
