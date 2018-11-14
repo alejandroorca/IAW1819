5dc3b0108abc84f39ed0e75db460c2a219309bed
alejandroorca <alejandro96esp@gmail.com>
2018-11-14 23:14:59 +0100

    Update

--- EjerciciosCadenas/src/es/cj/datos/Ejercicio5.java
+++ EjerciciosCadenas/src/es/cj/datos/Ejercicio5.java
@@ -7,8 +7,7 @@ public class Ejercicio5 {
 	private static Scanner sc = new Scanner(System.in);
 	private static String cadenaf = "";
 	private static String cadena;
-	private static String cadenan;
-	private static char abc [] = new char [26];
+	private static int cadenan;
 	private static int posicion;
 	public static void main(String[] args) {
 		// 5.Dada una cadena y un código entero aleatorio. 
@@ -20,35 +19,23 @@ public class Ejercicio5 {
 		} while (cadena.isEmpty());
 		do {
 			System.out.println("Introduce el valor numérico: ");
-			cadenan = sc.nextLine();
-		} while (cadenan.isEmpty() || cadenan.length() > cadena.length());
-		CrearABC();
+			cadenan = sc.nextInt();
+		} while (cadenan < 0);
 		System.out.println(ComparaYResuelve(cadena));
 	}
 	private static String ComparaYResuelve(String cadena2) {
 		for (int i = 0; i < cadena.length(); i++) {
-			char caracter = cadena.charAt(i);
-			if (caracter == abc[i]) {
-			posicion = i + Character.getNumericValue(cadenan.charAt(i));
-			if (posicion < 27 && posicion >= 0) {
-				cadenaf =""+abc[posicion];
-				posicion = 0;	
-			} else if (posicion >= 27) {
-				posicion = Character.getNumericValue(cadenan.charAt(i));
-				cadenaf =""+abc[posicion];
-				posicion=0;
-			}
-			
+			posicion = cadenan + Character.getNumericValue(cadena.charAt(i));
+			String letra = Integer.toBinaryString(posicion);
+			if (posicion < 27 && posicion > 0) {
+				cadenaf=""+letra;
+			} else {
+				cadenaf=""+cadenan;
 			} 
 		}
 		return cadenaf;
 		
 	}
-	private static void CrearABC() {
-		for (int i = 0; i < abc.length; i++) {
-			abc[i] = (char) ('a' + i);
-			System.out.print(abc[i]+ " ");
-}		
-	}
+
 
 }
